b804a7b984e18d067a71c01814f015ca7bc7c153
hanguyen6 <hanm6686@gmail.com>
2021-09-01 14:47:19 -0400

    Update lengthOfLongestSubstring.go

--- lengthOfLongestSubstring.go
+++ lengthOfLongestSubstring.go
@@ -40,3 +40,35 @@ func lengthOfLongestSubstring(s string) int {
     }
     return res
 }
+
+// Use sliding window to expand / contract the substring 
+// Use a hashset to store characters in current window.
+// slide window to the right 
+// if character in hashset, contract the windows from the left 
+// O(n) for time complexity 
+// O(min(m,n)) for space needed 
+func lengthOfLongestSubstring(s string) int {
+    
+    var chars [128]int
+    
+    res := 0 
+    left := 0
+    right := 0
+    
+    for right < len(s) {
+        r := s[right]
+        chars[r]++
+        
+        for chars[r] > 1 {
+            l := s[left]
+            chars[l]--
+            left++
+        }
+        
+        res = max(res, right - left + 1)
+        right++
+    }
+    
+    return res
+        
+}
