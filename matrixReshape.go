ebdd5e7039aa2651212c77d4bcb11251e773563e
hanguyen6 <hanm6686@gmail.com>
2021-09-03 15:13:17 -0400

    Refactor code without using queue

--- matrixReshape.go
+++ matrixReshape.go
@@ -31,3 +31,29 @@ func matrixReshape(mat [][]int, r int, c int) [][]int {
     }
     return m
 }
+
+// Refactor without using queue 
+func matrixReshape(mat [][]int, r int, c int) [][]int {
+    
+    // When to return original matrix 
+    if (len(mat) == 0 || r * c != len(mat) * len(mat[0])) {
+        return mat
+    }
+    
+    
+    // Rebuild new matrix
+    m := make([][]int, r)
+    for i := 0; i < r; i++ {
+        m[i] = make([]int, c)    
+    }
+    
+    
+    count := 0 
+    for i:=0; i < len(mat); i++ {
+        for j:=0; j < len(mat[0]); j++ {
+            m[count / c][ count % c] = mat[i][j]
+            count++
+        }
+    }
+    return m
+}
