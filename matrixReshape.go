5b8f13e889ba4912858486573091daf4d0422173
hanguyen6 <hanm6686@gmail.com>
2021-09-03 14:52:16 -0400

    solution with O(m*n) time & space complexity

---
+++ matrixReshape.go
@@ -0,0 +1,33 @@
+/** 
+* O(m*n) for time and space complexity 
+*/
+func matrixReshape(mat [][]int, r int, c int) [][]int {
+    
+    // When to return original matrix 
+    if (len(mat) == 0 || r * c != len(mat) * len(mat[0])) {
+        return mat
+    }
+    
+    // Extract all elements into a queue 
+    queue := []int{}
+    for i := 0; i < len(mat); i++ {
+        for j:=0; j < len(mat[i]); j++ {
+            queue = append(queue, mat[i][j])
+        }
+    }
+    
+    // Rebuild new matrix from the queue 
+    m := make([][]int, r)
+    for i := 0; i < r; i++ {
+        m[i] = make([]int, c)    
+    }
+    
+    for i:=0; i < r; i++ {
+        for j:=0; j < c; j++ {
+            head := queue[0]
+            queue = queue[1:]
+            m[i][j] = head     
+        }
+    }
+    return m
+}
