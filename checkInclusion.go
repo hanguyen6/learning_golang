14f40750b491d8bb41c62c01320f825c672f3f9c
hanguyen6 <hanm6686@gmail.com>
2021-09-09 14:31:37 -0400

    HashMap optimized solution

--- checkInclusion.go
+++ checkInclusion.go
@@ -45,3 +45,52 @@ func permute(s1 string, s2 string, l int) bool {
 func checkInclusion(s1 string, s2 string) bool {    
     return permute(s1, s2, 0)
 }
+
+/* Use HashMap/ByteArray to represent substring 
+* Consider all the substring of s2 with the same length as s1 
+* Populate hashMap of string -> occurence of the substrings or byte array to represent the substring
+* If the hashmap matched with the hashmap of s1 then return true 
+* O(1) for space need when using byte array to store the occurence of 26 character in string 
+* O(l1 + 26 * (l2-l1) * l1) for time complexity 
+*/
+
+func matches(s1map [26]byte, s2map [26]byte) bool{
+    for i:=0; i < 26; i++ {
+        if (s1map[i] != s2map[i]) {
+            return false
+        }
+    }
+    return true
+}
+
+func checkInclusion(s1 string, s2 string) bool {    
+    
+    if (len(s1) > len(s2)) {
+        return false 
+    }
+    
+    s1map := [26]byte{}
+    
+    // Update array of char byte with its occurence in s1 given s1 including only character a -> z
+    for i:=0; i < len(s1); i++ {
+        s1map[s1[i] - 'a']++
+    }
+    
+    fmt.Println("char array of s1: ", s1map)
+    
+    // Search s2's substring with len(s1) and calculate array of char byte -> occurence 
+    
+    for i:= 0; i <= len(s2) - len(s1); i++ {
+        s2map := [26]byte{}
+        
+        for j:=0; j < len(s1); j++ {
+            s2map[s2[i+j] - 'a']++
+        }
+        
+        if (matches(s1map, s2map)) {
+            return true
+        }
+    }
+    
+    return false
+}
