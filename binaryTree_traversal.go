0c4d58d55ffa37d15c494fe01b5a11fb62856bc8
hanguyen6 <hanm6686@gmail.com>
2021-09-11 17:33:23 -0400

    Update binaryTree_traversal.go

--- binaryTree_traversal.go
+++ binaryTree_traversal.go
@@ -73,3 +73,42 @@ func preorderTraversal(root *TreeNode) []int {
     return res
     
 }
+
+/**
+* Use a stack to contain visiting nodes 
+* Starting from root, 
+*   - Update stack with its left subTrees 
+*   - As poping element from the stack
+*        - Update its value to result list 
+*        - Visiting its right subtree, update it to stack 
+*/
+func inorderTraversal(root *TreeNode) []int {
+    if (root == nil) {
+        return []int{}
+    } 
+    
+    stack := make([]*TreeNode, 0)
+    fmt.Println("Initial stack ", stack)
+    curr := root 
+    res := make([]int, 0)
+    
+    for (curr != nil || len(stack) > 0) {        
+        // Keep adding left subtrees to stack 
+        for curr != nil {
+            stack = append(stack, curr)
+            curr = curr.Left
+        }
+        
+        // Poping element 
+        curr = stack[len(stack) - 1]
+        stack = stack[0:len(stack)-1]
+
+        // Update element to result list
+        res = append(res, curr.Val)
+        
+        // Visiting right subtree
+        curr = curr.Right
+        
+    }
+    return res
+}
